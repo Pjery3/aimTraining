6dcd4c8c881913c7a50accf8163fabf7c3e4e1a0
Pjery3 <mixxer1265755@gmail.com>
2023-12-28 17:06:58 +0300

    refactoring #1

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
+let score = document.querySelector('.counter span');
 document.addEventListener('DOMContentLoaded', function (e) {
     let circles = document.querySelectorAll('.circle');
-    let score = document.querySelector('.counter span');
-    let scoreCount = 0;
-    let createCircle = null;
-    circles.forEach((item, idx) => {
 
-        // item.addEventListener('click', function (e) {
-        //     this.remove();
-        //     scoreCount++;
-        // })
+    let scoreCount = 0;
+    circles.forEach((item, idx) => {
         score.innerHTML(scoreCount);
     })
     document.addEventListener('click', function (e) {
@@ -19,11 +14,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
         }
     })
 
-    let gameStart = document.querySelector('.start-gaming');
-    gameStart.addEventListener('click', function (e) {
-        this.classList.add('hidden');
-        startGame();
-    })
+
 
     document.addEventListener('keydown', function (e) {
         if (e.code == 'Escape') stopGame();
@@ -35,32 +26,60 @@ function startGame() {
     let getMaxLeftPosition = circlesContainer.clientWidth - 30;
     let getMaxTopPosition = circlesContainer.clientHeight - 30;
     let circleIndex = 0;
-
-    createCircle = setInterval(() => {
-        let leftPosition = randomInteger(15, getMaxLeftPosition);
-        let topPosition = randomInteger(15, getMaxTopPosition);
-        let div = document.createElement('div');
-        div.className = "circle";
-        div.style.cssText = `
-            top: ${topPosition}px;
-            left: ${leftPosition}px;
-            z-index: ${circleIndex++};
-        `;
-        circlesContainer.append(div);
-    }, 500)
 }
 
 function stopGame() {
-    let circlesContainer = document.querySelector('.circles');
-    circlesContainer.innerHTML = "";
-    clearInterval(createCircle);
-
-    let gameStart = document.querySelector('.start-gaming');
-    gameStart.classList.remove('hidden');
+    
 }
 
 function randomInteger(min, max) {
     // случайное число от min до (max+1)
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
+}
+const GAME_WINDOW = document.querySelector('.game-window');
+const CIRCLES_CONTAINER = document.querySelector('.circles');
+let cre
+const gameSetting = {
+    targetSize: 30,
+    rectangleSize: {
+        width: GAME_WINDOW.clientWidth,
+        height: GAME_WINDOW.clientHeight
+    },
+    spawnArea: {
+        top: randomInteger(gameSetting.targetSize, gameSetting.rectangleSize.height - gameSetting.targetSize),
+        left: randomInteger(gameSetting.targetSize, gameSetting.rectangleSize.width - gameSetting.targetSize),
+    }
+}
+
+class Game {
+    constructor() { 
+        let createCircle;
+     }
+    start(timeInterval) {
+        let gamePreview = document.querySelector('.start-gaming');
+        gamePreview.addEventListener('click', function (e) {
+            this.classList.add('hidden');
+        });
+
+        createCircle = setInterval(() => {
+            let div = document.createElement('div');
+            div.className = "circle";
+            div.style.cssText = `
+            top: ${topPosition}px;
+            left: ${leftPosition}px;
+            z-index: ${circleIndex++};
+        `;
+            circlesContainer.append(div);
+        }, timeInterval)
+    }
+
+    end(){
+        circlesContainer.innerHTML = "";
+        clearInterval(createCircle);
+        
+        let gameStart = document.querySelector('.start-gaming');
+        gameStart.classList.remove('hidden');
+        score.innerHTML = 0;
+    }
 }
